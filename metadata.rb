eb4f5c8d51c0322ab0b2f817343fa58b47f86036
Sean Carolan <scarolan@gmail.com>
2014-12-30 16:18:12 -0600

    Version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'scarolan@chef.io'
 license          'All rights reserved'
 description      'Installs an HA Chef cluster in AWS EC2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 depends          'lvm'
 depends          'aws'
